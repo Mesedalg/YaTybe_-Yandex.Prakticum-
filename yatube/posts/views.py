12791c97f471f6b6d2b838b56601d0e60b2cf7c0
Mesedalg <Mesedalg@gmail.com>
2022-01-08 15:35:34 +0300

    Created user form for writing new post

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post, Group
+from .forms import PostForm
 
 
 def index(request):
@@ -12,3 +13,17 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
+
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            author = request.user
+            text = form.cleaned_data['text']
+            group = form.cleaned_data['group']
+            post = Post(author=author,text=text,group=group)
+            post.save()
+            return redirect('index')
+        return render(request, 'new_post.html', {'form': form})
+    form = PostForm()
+    return render(request, 'new_post.html', {'form': form})
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -2,10 +2,21 @@ from django.views.generic import CreateView
 
 from django.urls import reverse_lazy
 
-from .forms import CreationForm
+from .forms import CreationForm, ContactForm
+from django.shortcuts import redirect, render
 
 
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+
+def user_contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            return redirect('/thank-you/')
+        return render(request, 'post.html', {'form': form})
+    form = ContactForm()
+    return render(request, 'post.html', {'form': form})
